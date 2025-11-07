450909ce38065adacc12ec44259bee1df3ec9776
smokerm7 <166821045+smokerm7@users.noreply.github.com>
2025-11-07 09:42:09 +0300

    Refactor function names and update demo prints

--- Khizriev ASA/lab-03/src/main.py
+++ Khizriev ASA/lab-03/src/main.py
@@ -1,23 +1,22 @@
 # main.py
 
-from modules.recursion import factorial, fibbonachi, quick_power
-from modules.memoization import compare_fibbonachi, Visualization
-from modules.recursion_tasks import binary_search_recursive, print_directory_tree, hanoi
+from modules.recursion import factorial, fibonacci, fast_power
+from modules.memoization import compare_fib, visualize_fib
+from modules.recursion_tasks import binary_search_rec, print_tree, hanoi
 
-# Демонстрация работы функций из модуля recursion
+# Recursion demo
 print(factorial(1), factorial(5), factorial(7), factorial(9), factorial(-1))
-print(fibbonachi(1), fibbonachi(5), fibbonachi(
-    7), fibbonachi(15), fibbonachi(-1))
-print(quick_power(5, 7), quick_power(2, 21), quick_power(2, 65))
+print(fibonacci(1), fibonacci(5), fibonacci(7), fibonacci(15), fibonacci(-1))
+print(fast_power(5, 7), fast_power(2, 21), fast_power(2, 65))
 
-# Демонстрация работы функций из модуля memoization
-print(compare_fibbonachi(35))
+# Memoization demo
+print(compare_fib(35))
 
-# Демонстрация работы функций из модуля recursion_tasks
+# Recursion tasks demo
 arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-print(binary_search_recursive(arr, 7, 0, len(arr)-1))
-print_directory_tree("./src")
-hanoi(3, 'A', 'C', 'B')
+print(binary_search_rec(arr, 7, 0, len(arr)-1))
+print_tree("./src")
+hanoi(3, "A", "C", "B")
 
-# Демонстрация работы функции визуализации из модуля memoization
-Visualization([5, 10, 15, 20, 25, 30, 35])
+# Fibonacci visualization
+visualize_fib([5, 10, 15, 20, 25, 30, 35])
